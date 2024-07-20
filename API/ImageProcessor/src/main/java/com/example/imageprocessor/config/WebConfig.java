8a37cfc5bd5f773b36a08291838dfd223ffd279d
ashutosh5786 <ashutosh5786@outlook.com>
2024-07-20 18:56:01 +0100

    modified the webview

--- API/ImageProcessor/src/main/java/com/example/imageprocessor/config/WebConfig.java
+++ API/ImageProcessor/src/main/java/com/example/imageprocessor/config/WebConfig.java
@@ -2,7 +2,6 @@ package com.example.imageprocessor.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,15 +11,5 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         // Allow all origins for simplicity in local development
         registry.addMapping("/**").allowedOrigins("*");
-    }
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/{spring:[a-zA-Z0-9-_]+}")
-                .setViewName("forward:/index.html");
-        registry.addViewController("/**/{spring:[a-zA-Z0-9-_]+}")
-                .setViewName("forward:/index.html");
-        registry.addViewController("/{spring:[a-zA-Z0-9-_]+}/**{spring:?!(\\.js|\\.css|\\.png|\\.jpg|\\.jpeg|\\.gif|\\.svg|\\.woff|\\.woff2|\\.ttf|\\.eot)$}")
-                .setViewName("forward:/index.html");
     }
 }
